fbe6c19de3ad733d80cd7afa9c5873b4faa6565f
Caleb Bassi <calebjbassi@gmail.com>
2018-12-03 22:11:22 -0800

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='rofi-files',
-    version='0.1.0',
+    version='0.1.1',
     scripts=['rofi-files'],
     url='https://github.com/cjbassi/rofi-files',
 )
